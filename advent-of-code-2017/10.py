0126e29d8682247dd3328f1844bfb4985e1db596
Daniel Cardoza <dan@danielcardoza.com>
2022-07-20 23:54:52 -0400

    Solve P10 Part 2 for AOC 2017

--- advent-of-code-2017/10.py
+++ advent-of-code-2017/10.py
@@ -11,3 +11,38 @@ for length in lengths:
     skip_length += 1
 
 print(init[0] * init[1])
+
+lengths = list(open("10.in").read().strip())
+#lengths = "1,2,3"
+lengths = [ord(length) for length in lengths]
+lengths.extend([17, 31, 73, 47, 23])
+
+print(lengths)
+init = list(range(0, 256))
+pos = 0
+skip_length = 0
+
+for it in range(64):
+    for length in lengths:
+        for j in range(length // 2):
+            tmp = init[(pos + j) % 256]
+            init[(pos + j) % 256] = init[(pos + length - 1 - j) % 256]
+            init[(pos + length - 1 - j) % 256] = tmp
+        pos += (length + skip_length) % 256
+        skip_length += 1
+print(init)
+
+xored  = [0] * 16
+for i in range(0, 256, 16):
+    for j in range(i, i+16):
+        xored[i // 16] ^= init[j]
+
+mapping = "0123456789abcdef"
+hash = ""
+for val in xored:
+    first = val >> 4
+    second = val & 15
+    hash += mapping[first]
+    hash += mapping[second]
+
+print(hash)
