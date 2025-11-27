4721d98871284998b13595dc1e422e6d093d0bf3
Daniel Cardoza <dan@danielcardoza.com>
2025-11-27 16:16:47 -0500

    cleaner

--- advent-of-code-2015/d7.py
+++ advent-of-code-2015/d7.py
@@ -2,63 +2,58 @@ import re
 import collections
 
 lines = open("d7.in").read().splitlines()
-# lines = open("d7.testin").read().splitlines()
 node_to_deps = collections.defaultdict(list)
 
 
 def do(lines):
-    d = dict()
+    inst = dict()
+    v = dict()
 
     def transform(m):
         for el in m.groups():
             if el.isdigit():
-                d[el] = lambda el=el: int(el)
+                inst[el] = lambda el=el: int(el)
         return m.groups()
 
     for line in lines:
         if m := re.match(r"(\w+) AND (\w+) -> (\w+)", line):
             m = transform(m)
-            d[m[2]] = lambda m=m: d[m[0]] & d[m[1]]
+            inst[m[2]] = lambda m=m: v[m[0]] & v[m[1]]
         elif m := re.match(r"(\w+) OR (\w+) -> (\w+)", line):
             m = transform(m)
-            d[m[2]] = lambda m=m: d[m[0]] | d[m[1]]
+            inst[m[2]] = lambda m=m: v[m[0]] | v[m[1]]
         elif m := re.match(r"(\w+) LSHIFT (\d+) -> (\w+)", line):
             m = transform(m)
-            d[m[2]] = lambda m=m: d[m[0]] << d[m[1]]
+            inst[m[2]] = lambda m=m: v[m[0]] << v[m[1]]
         elif m := re.match(r"(\w+) RSHIFT (\d+) -> (\w+)", line):
             m = transform(m)
-            d[m[2]] = lambda m=m: d[m[0]] >> d[m[1]]
+            inst[m[2]] = lambda m=m: v[m[0]] >> v[m[1]]
         elif m := re.match(r"NOT (\w+) -> (\w+)", line):
             m = transform(m)
-            d[m[1]] = lambda m=m: ~d[m[0]]
+            inst[m[1]] = lambda m=m: ~v[m[0]]
         elif m := re.match("(\w+) -> (\w+)", line):
             m = transform(m)
-            d[m[1]] = lambda m=m: d[m[0]]
+            inst[m[1]] = lambda m=m: v[m[0]]
         else:
             assert False
 
         for c in m[:-1]:
             node_to_deps[m[-1]].append(c)
 
-    ret = []
     start = "a"
-    seen = set()
 
-    def dfs(node):
+    def dfs(node, seen):
         if node in seen:
             return
         seen.add(node)
         for descendant in node_to_deps.get(node, []):
-            dfs(descendant)
-        ret.append(node)
+            dfs(descendant, seen)
+        v[node] = inst[node]()
+        v[node] &= 2**16 - 1
+        return v[node]
 
-    dfs(start)
-
-    for node in ret:
-        d[node] = d[node]()
-        d[node] &= 2**16 - 1
-
-    print(d[start])
+    val = dfs(start, set())
+    print(val)
 
 
 do(lines)
