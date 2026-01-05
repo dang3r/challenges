67f625bb2ea2a78819b7bbc33853304834119a54
Daniel Cardoza <dan@danielcardoza.com>
2026-01-05 17:26:36 -0500

    fibd

--- rosalind/fibd.py
+++ rosalind/fibd.py
@@ -13,9 +13,7 @@ for month in range(n):
         ages = {0: 1}
     else:
         ages = {age + 1: count for age, count in ages.items()}
-        ages[0] = sum(count for age, count in ages.items() if 1 < age)
-        if m in ages:
-            del ages[m]
+        ages[0] = sum(count for age, count in ages.items() if 1 < age <= m)
+        ages = {age: count for age, count in ages.items() if age < m}
     rabbits[month] = sum(ages.values())
-
 print(rabbits[-1])
